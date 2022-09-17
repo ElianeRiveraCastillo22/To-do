295b1f12c746cf47f5f75f5ed782cfcb00b8fcaa
Eliane <eliane@portal-labs.com>
2022-09-17 18:52:50 -0500

    add: estructura con componetes

---
+++ to-do/src/TodoCounter.js
@@ -0,0 +1,7 @@
+import React from "react";
+function TodoCounter(params) {
+    return(
+        <h2>Has completado 2 de 3 numeros</h2>
+    )
+}
+export {TodoCounter} 
---
+++ to-do/src/TodoSearch.js
@@ -0,0 +1,9 @@
+import React from "react";
+function TodoSearch(params) {
+  return (
+    <>
+      <input placeholder="Cebollas" />
+    </>
+  );
+}
+export { TodoSearch };
